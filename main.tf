7c2df79b695fe9a346af1ae08cba076d4cdcfe9d
kishimoto-banana <drehbleistift@gmail.com>
2020-02-02 18:30:50 +0900

    ECR

--- main.tf
+++ main.tf
@@ -1,8 +1,36 @@
 resource "aws_instance" "example" {
-    ami = "ami-0f9ae750e8274075b"
-    instance_type = "t1.micro"
+  ami           = "ami-0f9ae750e8274075b"
+  instance_type = "t1.micro"
 
-    tags = {
-        Name = "example"
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_ecr_repository" "example" {
+  name = "example"
+}
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.example.name
+
+  policy = <<EOF
+    {
+        "rules": [
+            {
+                "rulePriority": 1,
+                "description": "keep last 30 release tagged images",
+                "selection": {
+                    "tagStatus": "tagged",
+                    "tagPrefixList": ["release"],
+                    "countType": "imageCountMoreThan",
+                    "countNumber": 30
+                },
+                "action": {
+                    "type": "expire"
+                }
+            }
+        ]
     }
+    EOF
 }
